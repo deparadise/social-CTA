729b171ac8bc7d8672f714528e43fc687f5d5478
David Pineda <david@deparadise.com>
2016-03-27 18:22:54 -0700

    Started WordPress plugin development

---
+++ plugin-socialCTA.php
@@ -0,0 +1,10 @@
+<?php 
+/*Plugin Name: Social Call-To-Action
+Description: Sets up the control for the social-CTA widget.
+Version: 0.1
+Author: David Pineda_DEParadise Creative
+Author URI: http://www.deparadise.com
+License: GPLv2
+*/
+?>
+
