2902a82e7e3576319e9a3edc035c2b55151379f0
David Pineda <david@deparadise.com>
2016-03-27 18:29:52 -0700

    init plugin class structure

--- plugin-socialCTA.php
+++ plugin-socialCTA.php
@@ -6,5 +6,21 @@ Author: David Pineda_DEParadise Creative
 Author URI: http://www.deparadise.com
 License: GPLv2
 */
-?>
 
+class socialCTA extends WP_Widget {
+     
+    function __construct() {
+    }
+     
+    function form( $instance ) {
+    }
+     
+    function update( $new_instance, $old_instance ) {       
+    }
+     
+    function widget( $args, $instance ) {
+         
+    }
+     
+}
+?> <!-- END socialCTA plugin -->
